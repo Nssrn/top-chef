1cd414f7e7c5a494024a95112d5f7e3cf9fd566e
Nissrine <anissrine95@hotmail.fr>
2018-03-01 16:29:48 +0100

    feat(restaurant) : module michelin

--- src/michelin.js
+++ src/michelin.js
@@ -2,42 +2,77 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
-if (fs.existsSync('./restau_list.json')){
-  fs.truncate('restau_list.json', 0, function(){
-  })
-}
 
-var Reader = require('readline').createInterface({
-  input: require('fs').createReadStream('./links.txt')
-});
+function get_info(uri, callback) {
+  request(uri, function(error, response, body){
+    if(!error) {
 
-Reader.on('line', function(line){
-  request({
-    uri: line,
-  }, function(error, response, body) {
-    if(error) return console.log(error);
-    
-    var $ = cheerio.load(body);
-    var restaurant = {};
-    restaurant['name'] = $('.poi_intro-display-title').text().trim();
-
+    const $ = cheerio.load(body);
+    var name = $('.poi_intro-display-title').text().trim();
     var thoroughfare = $('.poi_intro-display-address .field__item .thoroughfare').text();
     var postalcode = $('.poi_intro-display-address .field__item .postal-code').text();
     var locality = $('.poi_intro-display-address .field__item .locality').text();
+    
+    var restaurant = {};
+    restaurant['name'] = name;
     var address = {};
     address['thoroughfare'] = thoroughfare;
     address['postalcode'] = postalcode;
     address['locality'] = locality;
     restaurant['address'] =  address;
+    restaurant['link']= uri;
 
-    
-    try{
-      fs.appendFileSync("restau_list.json",JSON.stringify(restaurant) + "\n");
-    }catch(err){
-      console.log(err)
+    callback(restaurant);
+  }
+
+  });
+}
+
+function get_links(uri, callback){
+  var links = [];
+  request(uri, function(error, reponse, body){
+    if(!error) {
+      const $ = cheerio.load(body);
+      $('.poi-card-link').each(function(index, element){
+        links.push("https://restaurant.michelin.fr" + $(element).attr('href'));
+    });
+    callback(links);
+  }
+  });
+
+}
+
+function scrape(uri){
+  var json = { "restaurants" : []};
+  var pages = 36;
+  for (var i = 1; i < pages; i++){
+    get_links(uri + '/page-' + i, function(arr) {
+      arr.forEach(function(element) {
+        get_info(element, function(restaurant){
+          json.restaurants.push(restaurant);
+          fs.writeFile('restau_list.json', JSON.stringify(json), 'utf8',function(error){
+            if (!error){
+              console.log('Restaurant added succesufully');
+            }
+            else {return console.log(error)};
+          });
+        }); 
+      });
+    });
+  }
+}
+
+function get(){
+  if (!fs.existsSync('./restau_list.json')){
+    scrape("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin");
+    return console.log('In progress...');
     }
+    var content = fs.readFileSync('./restau_list.json', 'utf-8');
+    return JSON.parse(content);
 
-  }).on('error', function(err){console.log(err)}
-).end()
-});
+  }
+
+  exports.get=get;
--- src/server.js
+++ src/server.js
@@ -1,55 +1,14 @@
 var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
 var app = express();
+var michelin = require('./michelin');
 
-
-
-    request ({
-        uri: "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin",
-    }, function(error, response, body) {
-        var $ = cheerio.load(body);
-
-        //if file exists delete the content
-        if(fs.existsSync('links.txt')) {
-            fs.truncate('links.txt', 0, function(){
-            
-            })
-        }
+var json = michelin.get();
+app.get('/restau_list', function(req,res){
+    res.send(json);
     
-
-    var pages = 36;
-    for(i =1; i < pages; i ++){
-
-    
-    request({
-        uri: "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-" +i,
-    }, function (error, response, body) {
-         
-            var $ = cheerio.load(body);
-            $('.poi-card-link').each(function(index){
-                var link = $(this);
-                //console.log(link);
-                var restau_link = "https://restaurant.michelin.fr" + link.attr('href');
-                try{
-                    fs.appendFile("links.txt", restau_link + "\n");
-                } catch(err){
-                    console.log(err);
-                }
-            });
-
-        }).on('error', function(err){
-            console.log(err)
-        }).end()
-
-        }
-    }).on('error', function(err) {
-        console.log(err)
-    }).end();
-
+})
 
 
 //app.listen('8081')
-console.log('Magic happens on port 8081');
+//console.log('Magic happens on port 8081');
 exports = module.exports = app;
