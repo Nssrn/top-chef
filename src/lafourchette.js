4650cfa2d4cf1c4de9112fe150da87c990682dc1
Nissrine <anissrine95@hotmail.fr>
2018-03-02 15:16:40 +0100

    test API on laForuchette

--- src/lafourchette.js
+++ src/lafourchette.js
@@ -2,3 +2,51 @@ var cheerio = require('cheerio');
 var request = require('request');
 var fs = require('fs');
 
+
+//1ere API : rechercher les noms et codepostal et récuperer les id
+function search_id(){
+    var uri = "https://m.lafourchette.com/api/restaurant-prediction?name=LeRadio";
+    request(uri,function(error,response,html){
+        if(!error){
+            var $ = cheerio.load(html);
+            const element =$('body');
+            var test = element.text();
+            var objectValue = JSON.parse(test);
+
+            console.log(objectValue[0].id);
+        }
+    });
+
+}
+//search_id();
+
+//2eme api : avec les id récuperer les promos 
+
+function search_promo(){
+    var uri = "https://m.lafourchette.com/api/restaurant/78372/sale-type";
+
+    request(uri,function(error,response,html){
+        if(!error){
+            var $ = cheerio.load(html);
+           
+                const element =$('body');
+                var test = element.text();
+                var objectValue = JSON.parse(test);
+
+               for (var i = 0; i < objectValue.length; i++){
+                   if (objectValue[i].is_special_offer == true){
+                    console.log(objectValue[i].title);
+
+                   }
+                
+               }
+
+        }
+    });
+
+}
+
+
+search_promo();
+
+
