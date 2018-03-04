18c9e0fb7199cabdbf2d420564c16d35a0c58740
Nissrine <anissrine95@hotmail.fr>
2018-03-04 11:53:00 +0100

    feat(restaurant) : return the list of restaurant from Michelin

--- src/lafourchette.js
+++ src/lafourchette.js
@@ -3,50 +3,112 @@ var request = require('request');
 var fs = require('fs');
 
 
+var data = fs.readFileSync("./RestaurantInfo.json", "utf-8");
+var restaurant = data.split("\n");
+
+var contents = [];
+var results = [];
+
+//console.log(restaurant.length);
+for(var i =0; i<restaurant.length - 1; i++){
+     contents[i]= JSON.parse(restaurant[i]);
+    
+}
+
 //1ere API : rechercher les noms et codepostal et récuperer les id
-function search_id(){
-    var uri = "https://m.lafourchette.com/api/restaurant-prediction?name=LeRadio";
+function getDeal(){
+    console.log("In progress...");
+    for(var i = 0; i<contents.length; i++){
+        let search = contents[i].name;
+        let postalcode = contents[i].postalcode;
+        let stars = contents[i].stars;
+        let locality = contents[i].locality;
+        let picture = contents[i].picture;
+    
+    var uri = "https://m.lafourchette.com/api/restaurant-prediction?name=" + encodeURIComponent(search)
+
     request(uri,function(error,response,html){
         if(!error){
-            var $ = cheerio.load(html);
-            const element =$('body');
-            var test = element.text();
-            var objectValue = JSON.parse(test);
+            const $ = cheerio.load(html);
+            results = JSON.parse($.text().trim());
+            //console.log(results);
+            
+            var idF = getIdResults(postalcode);
+            //console.log(idF);
 
-            console.log(objectValue[0].id);
+            let tab = [];
+            let hasPromo = false;
+            if (idF != undefined) {
+                search_promo(idF, search, stars,locality,picture);
+            }
+
         }
     });
 
 }
-//search_id();
+}
+
+function getIdResults(postal_code) {
+    
+
+    for (var i = 0; i< results.length; i++){
+        if(results[i].address.postal_code == postal_code){
+            var idResuslts = results[i].id;
+            return idResuslts;
+        }
+    }
+}
+
+
+
 
 //2eme api : avec les id récuperer les promos 
 
-function search_promo(){
-    var uri = "https://m.lafourchette.com/api/restaurant/78372/sale-type";
+function search_promo(idF,name,stars, locality, picture){
+    let promo =[];
+    var uri = "https://m.lafourchette.com/api/restaurant/" + idF + "/sale-type";
 
     request(uri,function(error,response,html){
         if(!error){
             var $ = cheerio.load(html);
-           
-                const element =$('body');
-                var test = element.text();
-                var objectValue = JSON.parse(test);
+            resultsSales = JSON.parse($.text().trim());
+            if (idF != undefined){
+                promo = addPromo(resultsSales, promo);
+                if (promo.length != 0){
+                    //console.log("{Id : " +idF + "\nname : "+ name + "\npromo : " + promo +"}\n");
+                    restaurant = new Object();
+                    restaurant.idF = idF;
+                    restaurant.name = name;
+                    restaurant.promo = promo;
+                    restaurant.picture = picture;
+                    restaurant.locality = locality;
+                    restaurant.stars = stars;
+                    restaurant.link = "https://www.lafourchette.com/restaurant/" + encodeURIComponent(name) +"/" +idF;
+                    const doc = JSON.stringify(restaurant);
+                    fs.appendFileSync("./laFourchette.json", doc + "\r\n", 'utf8', (error) => {
+                        if (error) console.log(error);
+                    });                   
 
-               for (var i = 0; i < objectValue.length; i++){
-                   if (objectValue[i].is_special_offer == true){
-                    console.log(objectValue[i].title);
-
-                   }
-                
-               }
-
+                }
+            }
         }
+        
     });
 
 }
 
+function addPromo(resultsSales, promo) {
+    
+    for (var i = 0; i<resultsSales.length; i++){
+        if(resultsSales[i]["exclusions"] != "" && resultsSales[i].hasOwnProperty("exclusions")&&resultsSales[i]["is_special_offer"] == true){
+            promo.push(resultsSales[i]["title"].trim());
+        }
+    } return promo;
+}
 
-search_promo();
+getDeal();
+
+
+
 
 
--- src/michelin.js
+++ src/michelin.js
@@ -11,19 +11,14 @@ function get_info(uri, callback) {
     if(!error) {
 
     const $ = cheerio.load(body);
-    var name = $('.poi_intro-display-title').text().trim();
-    var thoroughfare = $('.poi_intro-display-address .field__item .thoroughfare').text();
-    var postalcode = $('.poi_intro-display-address .field__item .postal-code').text();
-    var locality = $('.poi_intro-display-address .field__item .locality').text();
-    
-    var restaurant = {};
-    restaurant['name'] = name;
-    var address = {};
-    address['thoroughfare'] = thoroughfare;
-    address['postalcode'] = postalcode;
-    address['locality'] = locality;
-    restaurant['address'] =  address;
-    restaurant['link']= uri;
+    var restaurant = new Object();
+    restaurant.name = $('.poi_intro-display-title').text().trim();
+    restaurant.stars = $('#node_poi-guide-wrapper > div.node_poi-distinction-section > ul > li:nth-child(1) > div.content-wrapper').text();
+    restaurant.thoroughfare = $('.poi_intro-display-address .field__item .thoroughfare').text();
+    restaurant.postalcode = $('.poi_intro-display-address .field__item .postal-code').text();
+    restaurant.locality = $('.poi_intro-display-address .field__item .locality').text();
+    restaurant.picture = $(".landscape").attr("data-src");
+    restaurant.link = uri; 
 
     callback(restaurant);
   }
@@ -46,14 +41,14 @@ function get_links(uri, callback){
 }
 
 function scrape(uri){
-  var json = { "restaurants" : []};
+  //var json = { "restaurants" : []};
   var pages = 36;
   for (var i = 1; i < pages; i++){
     get_links(uri + '/page-' + i, function(arr) {
       arr.forEach(function(element) {
         get_info(element, function(restaurant){
-          json.restaurants.push(restaurant);
-          fs.writeFile('restau_list.json', JSON.stringify(json), 'utf8',function(error){
+          const doc = JSON.stringify(restaurant);
+          fs.appendFileSync("./RestaurantInfo.json", doc + "\r\n", 'utf8', function(error){
             if (!error){
               console.log('Restaurant added succesufully');
             }
@@ -65,14 +60,17 @@ function scrape(uri){
   }
 }
 
+
 function get(){
-  if (!fs.existsSync('./restau_list.json')){
+  if (!fs.existsSync('./RestaurantInfo.json')){
     scrape("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin");
     return console.log('In progress...');
     }
-    var content = fs.readFileSync('./restau_list.json', 'utf-8');
+    var content = fs.readFileSync('./RestaurantInfo.json', 'utf-8');
     return JSON.parse(content);
 
-  }
+}
 
-  exports.get=get;
+get();
+
+
